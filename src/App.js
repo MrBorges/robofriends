dff2239339ab1e645ed3d67daa6609de19093452
Lucas Borges <lucas.info@gmail.com>
2019-06-03 20:58:51 +0000

    Previous to the last session of the course.

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './Robots';
+
+class App extends Component {
+	constructor(){
+					super()
+					this.state = {
+						robots: [],
+						searchfield: ''
+			}
+		}	
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+			return response.json();
+		})
+		.then(users => {
+			this.setState({ robots: users });
+		})
+		
+	}
+
+	onSearchChange = (event) => {		
+		this.setState({ searchfield: event.target.value });			
+	}
+
+	render(){
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+
+		return (
+				<div className='tc'>
+					<h1>Robofriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots} />
+				</div>
+				);
+	}
+}
+			  
+
+
+export default App;
---
+++ src/Robots.js
@@ -0,0 +1,73 @@
+export const robots =[
+
+{
+	id: 1,
+	name: 'Lucas Borges',
+	username: 'lucas',
+	email: 'lucas@email.com'
+},
+
+{
+	id: 2,
+	name: 'Inácio Cardoso Borges',
+	username: 'inacio',
+	email: 'inacio@email.com'
+},
+
+{
+	id: 3,
+	name: 'Eloá Cardoso O. Borges',
+	username: 'eloa',
+	email: 'eloa@email.com'
+},
+
+{
+	id: 4,
+	name: 'Benedito Borges',
+	username: 'benedito',
+	email: 'benedito@email.com'
+},
+
+{
+	id: 5,
+	name: 'Sônia M. S. Borges',
+	username: 'sonia',
+	email: 'sonia@email.com'
+},
+
+{
+	id: 6,
+	name: 'Gilberto Figueiredo',
+	username: 'gilberto',
+	email: 'gilberto@email.com'
+},
+
+{
+	id: 7,
+	name: 'Patricia Lotufo',
+	username: 'patricia',
+	email: 'patricia@email.com'
+},
+
+{
+	id: 8,
+	name: 'Luciano Biriberi',
+	username: 'luciano',
+	email: 'luciano@email.com'
+},
+
+{
+	id: 9,
+	name: 'Gilmar Lefigaro',
+	username: 'gilmar',
+	email: 'gilmar@email.com'
+},
+
+{
+	id: 10,
+	name: 'Isabela Legusta',
+	username: 'isabela',
+	email: 'isabela@email.com'
+},
+
+]
