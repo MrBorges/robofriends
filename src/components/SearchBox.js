8bf7703a89f2a7561d45f399746b422c1a3355b0
MrBorges <lucas.info@gmail.com>
2021-10-28 10:35:34 -0300

    Changed search placeholder

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const searchBox = ({ searchChange })=>
 	{
 		return (
 			<div className="pa2">
-				<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search robots" onChange={searchChange}/>
+				<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search friends" onChange={searchChange}/>
 			</div>
 		);
 }
